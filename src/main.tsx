e12228aef5f4b06a02650ee57d7558c7fa42e839
H-Ngyen <hoangnguyennn0210@gmail.com>
2025-03-06 10:20:05 +0700

    Add: layout, header,hero

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './globals.css'
-import App from './App.tsx'
+import './global.css'
 import AppRoutes from "./AppRoutes.tsx"
 
 createRoot(document.getElementById('root')!).render(
